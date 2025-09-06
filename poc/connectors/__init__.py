2ca14b27e8e33a2c2c2af9a890b95c0b24f9e902
kevin-toles <kevin.toles.2024@gmail.com>
2025-09-06 11:19:59 -0700

    Delete poc directory

--- poc/connectors/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Re-export connector fetchers for easy import in pipelines."""
-from .remoteok import fetch as fetch_remoteok
-from .greenhouse import fetch as fetch_greenhouse
-from .lever import fetch as fetch_lever
-
-__all__ = ["fetch_remoteok", "fetch_greenhouse", "fetch_lever"]
